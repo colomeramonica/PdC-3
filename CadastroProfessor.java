f20f9ff492fe5fc39e97bf633d33b82f29bd692c
Monica Colomera <colomeramonica@gmail.com>
2018-05-28 18:04:19 -0300

    Validacao dos campos

--- CadastroProfessor.java
+++ CadastroProfessor.java
@@ -17,22 +17,23 @@ import javax.swing.JOptionPane;
 
 public class CadastroProfessor extends JFrame {
 	public List<Professor> lstProfessores;
+	private boolean navegacao;
 	public CadastroProfessor() {
 		super("Teste");
 		
 		lstProfessores = new LinkedList<>();
 		setLayout(new GridLayout(5,2));
 		
-		JLabel lblNomeProf = new JLabel("Nome: ");
+		JLabel lblNomeProf = new JLabel("*Nome: ");
 		JTextField txtNomeProf = new JTextField();
 		
 		JLabel lblEspecialidade = new JLabel("*Especialidade: ");
 		JTextField txtEspecialidade = new JTextField();
 		
-		JLabel lblRgProf = new JLabel("RG: ");
+		JLabel lblRgProf = new JLabel("*RG: ");
 		JTextField txtRgProf = new JTextField();
 		
-		JLabel lblCpfProf = new JLabel("CPF: ");
+		JLabel lblCpfProf = new JLabel("*CPF: ");
 		JTextField txtCpfProf = new JTextField();
 		
 		JButton btCancelar = new JButton("Cancelar");
@@ -46,17 +47,35 @@ public class CadastroProfessor extends JFrame {
 		
 		btOk.addActionListener((e)->{
 			Professor p = new Professor();
+			if(txtNomeProf.getText().equals("")){
+				JOptionPane.showMessageDialog(null,"Voce deve informar o nome!");
+			}
 			p.setNome(txtNomeProf.getText());
-			p.setNome(txtNomeProf.getText());
+	
+			if(txtRgProf.getText().equals("")){
+				JOptionPane.showMessageDialog(null,"Voce deve informar o RG!");
+			}
 			p.setRg(txtRgProf.getText());
+			
+			if(txtCpfProf.getText().equals("")){
+				JOptionPane.showMessageDialog(null,"Voce deve informar o CPF!");
+			}
 			p.setCpf(txtCpfProf.getText());
 			
 			if(txtEspecialidade.getText().equals("")){
-				JOptionPane.showMessageDialog(null,"Voce deve informar a especialidade");
+				JOptionPane.showMessageDialog(null,"Voce deve informar a especialidade!");
 			}
 			p.setEspecialidade(txtEspecialidade.getText());
+			
 			lstProfessores.add(p);
 			dispose();
+		});
+		
+		btOk.addActionListener((e)->{
+			
+			
+			
+			
 		});
 		
 		btCancelar.addActionListener((e)->{ dispose(); });
