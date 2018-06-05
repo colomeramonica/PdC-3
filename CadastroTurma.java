cf307dbfda07bd2f9d5c1e63a2bd8a86cbe6d684
Monica Colomera <colomeramonica@gmail.com>
2018-06-05 02:07:32 -0300

    Atualizacao na janela de cadastro de turmas. É necessario pegar o professor selecionado e adiciona-lo a turma

--- CadastroTurma.java
+++ CadastroTurma.java
@@ -5,19 +5,22 @@ import java.awt.*;
 public class CadastroTurma extends JDialog{
 
     private JFrame jFrame;
-    private JPanel panel1;
-    private JPanel panel2;
-    private JPanel panel3;
-    private JPanel panel4;
-    private JPanel panelButo;
-    private JButton butao;
-    private JButton butaoAdd;
-    private JButton butaoRem;
-    private JTextField textFieldNome;
-    private JTextField textFieldSobrenome;
+    private JPanel panelInicio;
+    private JPanel panelFim;
+    private JPanel panelProf;
+    private JPanel panelList;
+    private JPanel panelBotoes;
+    private JPanel panelGerenciar;
+    private JButton btnProf;
+    private JButton btEsq;
+    private JButton btDir;
+    private JButton btOk;
+    private JButton btCancela;
+    private JTextField txtDataInicio;
+    private JTextField txtDataFim;
     private JTextField txtProfessor;
     private JList lstAdicionados;
-    private JList teste;
+    private JList listaAlunos;
     
 
     public CadastroTurma(Frame owner) {
@@ -30,68 +33,81 @@ public class CadastroTurma extends JDialog{
 		* que contém respectivamente o rótulo "Nome:" e o campo para edição. 
 		*/
 		 
-		panel1 = new JPanel();
-		panel1.setLayout(new GridLayout(1, 0));
+		panelInicio = new JPanel();
+		panelInicio.setLayout(new GridLayout(1, 0));
 		
-		panel1 = new JPanel();
-		panel1.setLayout(new GridLayout(1, 0));
+		panelInicio = new JPanel();
+		panelInicio.setLayout(new GridLayout(1, 0));
 		
-		panelButo = new JPanel();
-		panelButo.setLayout(new GridLayout(2, 1));
+		panelBotoes = new JPanel();
+		panelBotoes.setLayout(new GridLayout(2, 1));
 
 		/*
 		* Outro JPainel para adicionar os componentes JLabel e JTextField 
 		* que contém respectivamente o rótulo "Sobrenome:" e o campo para edição. 
 		*/
-		panel2 = new JPanel();
-		panel2.setLayout(new GridLayout(1, 0));
+		panelFim = new JPanel();
+		panelFim.setLayout(new GridLayout(1, 0));
 		
+		panelProf = new JPanel();
+		panelProf.setLayout(new GridLayout(1,3));
 		
-		panel3 = new JPanel();
-		panel3.setLayout(new GridLayout(1,3));
+		panelList = new JPanel();
+		panelList.setLayout(new GridLayout(1,3));
 		
-		panel4 = new JPanel();
-		panel4.setLayout(new GridLayout(1,3));
+		panelGerenciar = new JPanel();
+		panelGerenciar.setLayout(new GridLayout(1,0));
 
-		textFieldNome = new JTextField(15);
-		textFieldSobrenome = new JTextField(15);
-		txtProfessor = new JTextField();
-		butao = new JButton();
+		txtDataInicio = new JTextField(15);
+		txtDataFim = new JTextField(15);
+		btnProf = new JButton("Adicionar professor");
 		
 		lstAdicionados = new JList();
-		teste = new JList();
-		butaoAdd = new JButton();
-		butaoRem = new JButton();
+		listaAlunos = new JList();
+		txtProfessor = new JTextField();
+		btEsq = new JButton(">");
+		btDir = new JButton("<");
+		btOk = new JButton("Ok");
+		btCancela = new JButton("Cancelar");
 		
+		//Adição dos componentes ao JPanel
+		panelInicio.add(new JLabel("Data de Inicio: "));
+		panelInicio.add(txtDataInicio);
 
 		//Adição dos componentes ao JPanel
-		panel1.add(new JLabel("Data de Inicio: "));
-		panel1.add(textFieldNome);
-
-		//Adição dos componentes ao JPanel
-		panel2.add(new JLabel("Data Final: "));
-		panel2.add(textFieldSobrenome);
+		panelFim.add(new JLabel("Data Final: "));
+		panelFim.add(txtDataFim);
 		
-		panel3.add(new JLabel("Selecionar professor:"));
-		panel3.add(txtProfessor);
-		panel3.add(butao);
+		panelProf.add(new JLabel("Selecionar professor:"));
+		panelProf.add(txtProfessor);
+		panelProf.add(btnProf);
+		txtProfessor.setVisible(false);
 		
-		panel4.add(lstAdicionados);
-		panel4.add(panelButo);
-		panel4.add(teste);
+		panelList.add(lstAdicionados);
+		panelList.add(panelBotoes);
+		panelList.add(listaAlunos);
 		
-		panelButo.add(butaoAdd);
-		panelButo.add(butaoRem);
+		panelBotoes.add(btEsq);
+		panelBotoes.add(btDir);
 		
+		panelGerenciar.add(btOk);
+		panelGerenciar.add(btCancela);
 		
-
 		//Adição dos componentes JPanel ao JFrame
-		jFrame.add(panel1);
-		jFrame.add(panel2);
-		jFrame.add(panel3);
-		jFrame.add(panel4);
+		jFrame.add(panelInicio);
+		jFrame.add(panelFim); jFrame.add(panelProf); 
+		jFrame.add(panelList); jFrame.add(panelGerenciar);
+		
+		btnProf.addActionListener((e)->{
+			JanelaProfessor j = new JanelaProfessor(null);
+		});
 		
 		jFrame.setSize(500,300);
 		jFrame.setVisible(true);
     }
+    
+    public void selecionarProfessor(Professor p) {
+			txtProfessor.setText(p.getNome());
+			txtProfessor.setVisible(true);
+		}
 }
