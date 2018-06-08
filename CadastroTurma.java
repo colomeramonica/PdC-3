4168886e479b037bc5a02328c8221ef5a738e2de
Monica Colomera <colomeramonica@gmail.com>
2018-06-08 03:25:37 -0300

    Uma bela turminha com belos botoenzinhos que mexem as pessoinhas

--- CadastroTurma.java
+++ CadastroTurma.java
@@ -1,63 +1,100 @@
-/* Tela de cadastro de novas turmas */
-
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
 import java.util.List;
 
 public class CadastroTurma extends JDialog{
+
+    private JFrame jFrame;
+    private JPanel panelInicio;
+    private JPanel panelFim;
+    private JPanel panelProf;
+    private JPanel panelList;
+    private JPanel panelBotoes;
+    private JPanel panelGerenciar;
+    private JButton btnProf;
+    private JButton btEsq;
+    private JButton btDir;
+    private JButton btOk;
+    private JButton btCancela;
+    private JTextField txtDataInicio;
+    private JTextField txtDataFim;
+    private JTextField txtProfessor;
     private JList<Aluno> listaAdicionados;
     private JList<Aluno> listaAlunos;
-    private ArrayList<Aluno> alunos;
-    private ArrayList<Turma> turma;
-    private List <Aluno> lstAux;
-    private int size;
- 
+    private List<Aluno> selected;
+    private Turma turmaAux;
+    private Professor professor;
+    private ArrayList <Aluno> alunos;
+
     public CadastroTurma(Frame owner) {
 		super(owner,true);
-		JFrame jFrame = new JFrame("Gerenciar turmas");
-		jFrame.setLayout(new GridLayout(5,5)); // setando o layout inicial da tela 
-
-		/** Declaração dos panels que serão usados **/
-		JPanel panelInicio = new JPanel(); // Panel para a adição dos JLabel e JTextField referentes a Data Inicial
-		JPanel panelFim = new JPanel(); // Panel para a adição dos JLabel e JTextField referentes a Data Final
-		JPanel panelBotoes = new JPanel(); // Panel para a adição dos JButton referentes aos botões '<' e '>'
-		JPanel panelProf = new JPanel(); // Panel referente ao JButton de escolha do professor e o JTextField que exibe o nome do mesmo
-		JPanel panelList = new JPanel(); // Panel para a adição do JList que irá exibir os alunos cadastrados
-		JPanel panelGerenciar = new JPanel();
+		jFrame = new JFrame("Gerenciar turmas");
+		jFrame.setLayout(new GridLayout(5,5));
+	
+		/*
+		* Um JPainel para adicionar os componentes JLabel e JTextField 
+		* que contém respectivamente o rótulo "Nome:" e o campo para edição. 
+		*/
+		 
+		panelInicio = new JPanel();
+		panelInicio.setLayout(new GridLayout(1, 0));
 		
-		/** Configuração dos layouts de cada panel **/
-		panelInicio.setLayout(new GridLayout(1, 0)); 
+		panelInicio = new JPanel();
+		panelInicio.setLayout(new GridLayout(1, 0));
+
+		
+		panelBotoes = new JPanel();
 		panelBotoes.setLayout(new GridLayout(2, 1));
+
+		/*
+		* Outro JPainel para adicionar os componentes JLabel e JTextField 
+		* que contém respectivamente o rótulo "Sobrenome:" e o campo para edição. 
+		*/
+		panelFim = new JPanel();
 		panelFim.setLayout(new GridLayout(1, 0));
+		
+		
+		panelProf = new JPanel();
 		panelProf.setLayout(new GridLayout(1,3));
+		
+		panelList = new JPanel();
 		panelList.setLayout(new GridLayout(1,3));
+		
+		panelGerenciar = new JPanel();
 		panelGerenciar.setLayout(new GridLayout(1,0));
 
-		/** Declaração dos campos de texto **/
-		JTextField txtDataInicio = new JTextField();
-		JTextField txtDataFim = new JTextField();
-		JTextField txtProfessor = new JTextField();
+		txtDataInicio = new JTextField(15);
+		txtDataFim = new JTextField(15);
+		txtProfessor = new JTextField();
+		btnProf = new JButton("Selecionar professor: ");
+		btOk = new JButton("OK");
+		btCancela = new JButton("Cancelar");
 		
-		/** Declaração dos botões **/
-		JButton btEsq = new JButton(">");
-		JButton btDir = new JButton("<");
-		JButton btOk = new JButton("Ok");
-		JButton btCancela = new JButton("Cancelar");
-		JButton btnProf = new JButton("Adicionar professor");
+		listaAlunos = new JList();
+		listaAdicionados = new JList();
+		
+		btEsq = new JButton(">");
+		btDir = new JButton("<");
 		
 		/** Exibição dos alunos cadastrados **/
 		alunos = Dados.getInstance().getListAlunos();
-		
+
 		DefaultListModel<Aluno> lm = new DefaultListModel<>();
 		listaAlunos = new JList<>(lm);
 		listaAlunos.setModel(lm);
 
 		for(int i=0;i<alunos.size();i++){
-			lm.add(i,alunos.get(i));
+				lm.add(i,alunos.get(i));
 		} 
 		/** Fim da exibição dos alunos **/
 		
+		/**Adicionando alunos nas turmas **/
+		DefaultListModel<Aluno> lt = new DefaultListModel<>();
+		listaAdicionados = new JList<>(lt);
+		listaAdicionados.setModel(lt);
+		/** **/
+
 		//Adição dos componentes ao JPanel
 		panelInicio.add(new JLabel("Data de Inicio: "));
 		panelInicio.add(txtDataInicio);
@@ -69,7 +106,6 @@ public class CadastroTurma extends JDialog{
 		panelProf.add(new JLabel("Selecionar professor:"));
 		panelProf.add(txtProfessor);
 		panelProf.add(btnProf);
-		txtProfessor.setVisible(false);
 		
 		panelList.add(listaAlunos);
 		panelList.add(panelBotoes);
@@ -80,19 +116,22 @@ public class CadastroTurma extends JDialog{
 		
 		panelGerenciar.add(btOk);
 		panelGerenciar.add(btCancela);
-		
+
 		//Adição dos componentes JPanel ao JFrame
 		jFrame.add(panelInicio);
-		jFrame.add(panelFim); jFrame.add(panelProf); 
-		jFrame.add(panelList); jFrame.add(panelGerenciar);
-		
+		jFrame.add(panelFim);
+		jFrame.add(panelProf);
+		jFrame.add(panelList);
+		jFrame.add(panelGerenciar);
+
 		btnProf.addActionListener((e)->{
 		/** No clique do botão de 'Adicionar Professor' é aberta a tela de listagem de professores e,
 		 * ao selecionar um, o nome deste é exibido na tela de Turma e o botão é desabilitado para impedir a 
 		 * adição de mais professores em uma mesma turma **/
 			JanelaProfessor j = new JanelaProfessor(null);
 			try {
-				txtProfessor.setText(j.getSelected().getNome());
+				professor = j.getSelected();
+				txtProfessor.setText(professor.getNome());
 				txtProfessor.setVisible(true);
 				txtProfessor.setEditable(false);
 				btnProf.setEnabled(false);
@@ -103,17 +142,54 @@ public class CadastroTurma extends JDialog{
 		
 		/** Recebe os alunos selecionados e os adiciona na turma recém criada **/
 		btOk.addActionListener((e)->{
-			Turma turma = new Turma();
 		//@TODO jogar os alunos selecionados (que teoricamente estarão nessa nova lista) e adicioná-los na classe recém instanciada
+			Turma turmaAux = new Turma();
+			for(int i = 0; i < listaAdicionados.getModel().getSize(); i++) {
+				turmaAux.addAluno(listaAdicionados.getModel().getElementAt(i));
+			}
+			turmaAux.setProfessor(professor);
 			dispose();
 		}); 
+				
+		btEsq.addActionListener((e)->{
+			selected = listaAlunos.getSelectedValuesList();
+			try {
+				if (!lt.isEmpty()) {
+					for (int i = 0; i <= selected.size(); i++) {
+						lt.add(lt.size() - 1, selected.get(i));
+						lm.removeElementAt(i);
+					}
+			} else {
+				for (int i = 0; i <= selected.size(); i++) {
+						lt.add(i, selected.get(i));
+						lm.removeElementAt(i);
+					}
+				}
+			} catch (Exception ex) {
+				System.out.printf("");
+			}
+		});
 		
-		btCancela.addActionListener((e)->{
-			dispose();
+		btDir.addActionListener((e)->{
+			selected = listaAdicionados.getSelectedValuesList();
+			try {
+				if (!lm.isEmpty()) {
+					for (int i = 0; i <= selected.size(); i++) {
+						lm.add(lt.size() - 1, selected.get(i));
+						lt.removeElementAt(i);
+					}
+			} else {
+				for (int i = 0; i <= selected.size(); i++) {
+						lm.add(i, selected.get(i));
+						lt.removeElementAt(i);
+					}
+				}
+			} catch (Exception ex) {
+				System.out.printf("");
+			}
 		});
 		
 		jFrame.setSize(500,300);
 		jFrame.setVisible(true);
     }
-
 }
